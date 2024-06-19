a6ed06388f226ebe5ceab005e857e5b0d76be10f
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-06-20 00:10:14 +0200

    update main and add swiper

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menuCls = document.querySelector('.menu__close');
-const menuList = document.querySelector('.menu__list');
-const menuShadow = document.querySelector('.menu--close');
-menuBtn.addEventListener('click', ()=>{
-   menuList.classList.toggle('menu__list--open');
-   menuShadow.classList.toggle('menu--open');
+const menuBtn = document.querySelector(".menu__btn");
+const menuCls = document.querySelector(".menu__close");
+const menuList = document.querySelector(".menu__list");
+const menuShadow = document.querySelector(".menu--close");
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("menu__list--open");
+  menuShadow.classList.toggle("menu--open");
 });
 
-menuCls.addEventListener('click', () =>{
-   menuList.classList.remove('menu__list--open');
-   menuShadow.classList.remove('menu--open');
+menuCls.addEventListener("click", () => {
+  menuList.classList.remove("menu__list--open");
+  menuShadow.classList.remove("menu--open");
 });
 
+const opinionSwiper = new Swiper(".opinion-slider", {
+  spaceBetween:20,
+  slidesPerView: 1,
+  loop: true,
+  navigation: 
+  {
+    nextEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-next",
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 4,
+    },
+    662: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+  },
+});
